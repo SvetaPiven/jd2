07d596ecdb85589d5f14904ec9234efd3523c6f7
Sveta Piven <svetapiven93@icloud.com>
2023-04-01 12:31:11 +0300

    bag fix

--- src/main/java/com/avia/controller/mvc/PassengerController.java
+++ src/main/java/com/avia/controller/mvc/PassengerController.java
@@ -1,11 +1,19 @@
 package com.avia.controller.mvc;
 
+import com.avia.controller.requests.PassengerCreateRequest;
 import com.avia.domain.Passenger;
 import com.avia.repository.impl.PassengerRepositoryJdbcTemplateImpl;
+import com.avia.service.PassengerService;
 import lombok.RequiredArgsConstructor;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,17 +25,16 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
-
 @RequestMapping("/passengers")
 public class PassengerController {
 
     private static final Logger log = Logger.getLogger(PassengerController.class);
 
-    private final PassengerRepositoryJdbcTemplateImpl passengerRepositoryJdbcTemplate;
+    private final PassengerService passengerService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView findAll() {
-        List<Passenger> passengers = passengerRepositoryJdbcTemplate.findAll();
+    @GetMapping()
+    public String findAllPassenger() {
+        List<Passenger> passengers = passengerService.findAll();
 
         String collect = passengers.stream().map(Passenger::getFullName).collect(Collectors.joining(","));
 
@@ -35,13 +42,11 @@ public class PassengerController {
         modelAndView.addObject("passengerFullName", collect);
         modelAndView.addObject("passengers", passengers);
 
-        modelAndView.setViewName("hello");
-
-        return modelAndView;
+        return "index";
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ModelAndView findById(@PathVariable String id) {
+    @GetMapping("/{id}")
+    public String findByIdPassenger(@PathVariable String id) {
 
         Long parsedPassengerId;
 
@@ -52,14 +57,49 @@ public class PassengerController {
             parsedPassengerId = 1L;
         }
 
-        Optional<Passenger> passenger = passengerRepositoryJdbcTemplate.findOne(parsedPassengerId);
+        Optional<Passenger> passenger = passengerService.findOne(parsedPassengerId);
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("passengerFullName", passenger.getClass());
         modelAndView.addObject("passengers", Collections.singletonList(passenger));
 
-        modelAndView.setViewName("hello");
+        return "index";
+    }
 
-        return modelAndView;
+    @PutMapping
+    public String updatePassenger(@ModelAttribute PassengerCreateRequest request) {
+
+        Passenger passenger = passengerService.update(Passenger.builder()
+                .idPass(request.getIdPass())
+                .fullName(request.getFullName())
+                .personalId(request.getPersonalId())
+                .build());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("passenger", passenger);
+
+        return "update";
+    }
+
+    @PostMapping
+    public String createPassenger(@ModelAttribute PassengerCreateRequest request) {
+        Passenger passenger = Passenger.builder()
+                .fullName(request.getFullName())
+                .personalId(request.getPersonalId())
+                .build();
+        passengerService.create(passenger);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("passenger", passenger);
+
+        return "newpassenger";
+    }
+
+    @DeleteMapping
+    public String deletePassenger(@ModelAttribute PassengerCreateRequest request) {
+
+        passengerService.deleteById(request.getIdPass());
+
+        return "redirect:/passengers";
     }
 }
